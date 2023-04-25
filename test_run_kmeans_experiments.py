e5aa7568728b976cc542caeb262ca486dc8c1376
Marcos Carvalho <marcosnlc4@gmail.com>
2023-04-25 19:45:48 +0200

    Cleaning code for tests

--- test_run_kmeans_experiments.py
+++ test_run_kmeans_experiments.py
@@ -50,3 +50,50 @@ if __name__ == '__main__':
     #kmeans.fit(x)
     #compss_barrier()
     #print("==== TIME GPU ==== ", time.perf_counter()-start)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# # # CLEAN CODE (FOR TRACE GENERATION)
+#     input_matrix_rows = 32000
+#     input_matrix_columns = 32000
+#     start_random_state = 170
+#     block_row_size = 8000
+#     block_column_size = 8000
+#     n_clusters = 10
+
+#     shape = (input_matrix_rows, input_matrix_columns)
+#     block_size = (block_row_size, block_column_size)
+
+#     start = time.perf_counter()
+#     x = ds.random_array(shape, block_size, random_state=start_random_state)
+#     print("==== TIME DATA GENERATION ==== ", time.perf_counter()-start)
+
+#     # Run KMeans using dislib - CPU
+#     print("\nSTART CPU\n")
+#     # compss_barrier()
+#     start = time.perf_counter()
+#     kmeans = KMeans(n_clusters=n_clusters, random_state=start_random_state, id_device=1, max_iter=5, tol=0, arity=48)
+#     kmeans.fit(x)
+#     # compss_barrier()
+#     print("==== TIME CPU ==== ", time.perf_counter()-start)
+
+
+#     # Run KMeans using dislib - GPU
+#     #print("\nSTART GPU\n")
+#     ## compss_barrier()
+#     #start = time.perf_counter()
+#     #kmeans = KMeans(n_clusters=n_clusters, random_state=start_random_state, id_device=2, max_iter=5, tol=0, arity=48)
+#     #kmeans.fit(x)
+#     ## compss_barrier()
+#     #print("==== TIME GPU ==== ", time.perf_counter()-start)
--- test_run_matmul_experiments.py
+++ test_run_matmul_experiments.py
@@ -19,11 +19,11 @@ if __name__ == '__main__':
     writer.writerow(header)
     f.close()
 
-    input_matrix_rows = 4000
-    input_matrix_columns = 4000
+    input_matrix_rows = 32000
+    input_matrix_columns = 32000
     start_random_state = 170
-    block_row_size = 2000
-    block_column_size = 2000
+    block_row_size = 16000
+    block_column_size = 16000
     transpose_a = transpose_b = bl_transpose = False
     start = time.perf_counter()
     # x = ds.random_array((input_matrix_rows, input_matrix_columns), (block_row_size, block_column_size), random_state=start_random_state)
@@ -51,7 +51,7 @@ if __name__ == '__main__':
     print("\nSTART CPU\n")
     # compss_barrier()
     start = time.perf_counter()
-    result = ds.matmul(x, x, transpose_a, transpose_b, id_device=2, id_parameter=0, nr_algorithm_iteration=0)
+    result = ds.matmul(x, x, transpose_a, transpose_b, id_device=1, id_parameter=0, nr_algorithm_iteration=0)
     # compss_barrier()
     print("==== TIME CPU ==== ", time.perf_counter()-start)
 
@@ -86,3 +86,59 @@ if __name__ == '__main__':
     # result = ds.matmul(x, x, transpose_a, transpose_b, id_device=6, id_parameter=0, nr_algorithm_iteration=0)
     # # compss_barrier()
     # print("==== TIME GPU ==== ", time.perf_counter()-start)
+
+
+
+
+
+
+
+
+
+
+
+
+
+# # CLEAN CODE (FOR TRACE GENERATION)
+# import time
+# import csv
+# import dislib as ds
+# import numpy as np
+# from scipy import sparse as sp
+# from scipy.sparse import csr_matrix
+
+# from pycompss.api.api import compss_barrier
+
+# if __name__ == '__main__':
+
+
+#     input_matrix_rows = 32000
+#     input_matrix_columns = 32000
+#     start_random_state = 170
+#     block_row_size = 8000
+#     block_column_size = 8000
+#     transpose_a = transpose_b = bl_transpose = False
+#     start = time.perf_counter()
+
+#     shape = (input_matrix_rows, input_matrix_columns)
+#     block_size = (block_row_size, block_column_size)
+
+#     # DATASET GENERATION
+#     x = ds.random_array(shape, block_size, random_state=start_random_state)
+
+#     print("==== TIME DATA GENERATION ==== ", time.perf_counter()-start)
+
+    
+#     # Run Matmul using dislib - CPU
+#     print("\nSTART CPU\n")
+#     start = time.perf_counter()
+#     result = ds.matmul(x, x, transpose_a, transpose_b, id_device=1, id_parameter=0, nr_algorithm_iteration=0)
+#     print("==== TIME CPU ==== ", time.perf_counter()-start)
+    
+    
+
+#     # Run Matmul using dislib - GPU 
+#     #print("\nSTART GPU\n")
+#     #start = time.perf_counter()
+#     #result = ds.matmul(x, x, transpose_a, transpose_b, id_device=2, id_parameter=0, nr_algorithm_iteration=0)
+#     #print("==== TIME GPU ==== ", time.perf_counter()-start)
